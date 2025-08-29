74ca50dc27101464760517a8c5527e6991416144
CL221018-JayParmar <145004012+CL221018-JayParmar@users.noreply.github.com>
2025-08-29 14:13:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = 'bdc154ec3e6818b91a45ea863dc817b1'
+    SECRET_KEY = 'ENTER YOUR SECRET KEY '
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'data','employees.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'jay8085135710@gmail.com'
-    MAIL_PASSWORD = 'Jparmar@1234'
-    MAIL_DEFAULT_SENDER = 'jay8085135710@gmail.com'
+    MAIL_USERNAME = 'ENTER YOUR USERNAME'
+    MAIL_PASSWORD = 'ENTER YOUR PASSWORD'
+    MAIL_DEFAULT_SENDER ='ENTER EMAIL'
